4a94a64e035b9a5e173fb355683d589266d64759
xiaobiao.fan <xiaobiao.fan@hibobi.com>
2022-10-11 11:46:26 +0800

    bean初始化 增加简易属性填充

---
+++ simple-spring-step03/src/main/java/com/vinspier/biz/service/StockService.java
@@ -0,0 +1,7 @@
+package com.vinspier.biz.service;
+
+public interface StockService {
+
+    Long getStock(Long skuId);
+
+}
---
+++ simple-spring-step03/src/main/java/com/vinspier/biz/service/impl/StockServiceImpl.java
@@ -0,0 +1,15 @@
+package com.vinspier.biz.service.impl;
+
+import com.vinspier.biz.dao.StockDao;
+import com.vinspier.biz.service.StockService;
+
+public class StockServiceImpl implements StockService {
+
+    private StockDao stockDao;
+
+    @Override
+    public Long getStock(Long skuId) {
+        return null;
+    }
+
+}
